122f2560c1fb09f180465dd0793bbf3aecc86f03
Yoav Sabag <yoavsbg17@gmail.com>
2025-10-30 08:42:06 +0200

    Initial Commit

---
+++ BreathBreakRecordingTool/BreathBreakRecordingToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathBreakRecordingToolApp.swift
+//  BreathBreakRecordingTool
+//
+//  Created by Yoav Ss on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BreathBreakRecordingToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreathBreakRecordingTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BreathBreakRecordingTool
+//
+//  Created by Yoav Ss on 30/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
